a5a743089777e73f212fa035a533de9ea93695b6
Bhaskar100-hub <bhaskarwary7@gmail.com>
2023-02-28 20:27:09 +0530

    1) Created Framents and Linked the Fragments 2) Cretaed packages named Add List Update 3)Added Fragment Container in Main XML file

--- app/src/main/java/wary/bhaskar/myto_doapp/fragments/list/ListFragment.kt
+++ app/src/main/java/wary/bhaskar/myto_doapp/fragments/list/ListFragment.kt
@@ -1,10 +1,11 @@
-package wary.bhaskar.myto_doapp
+package wary.bhaskar.myto_doapp.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import wary.bhaskar.myto_doapp.R
 
 class ListFragment : Fragment() {
     override fun onCreateView(
--- app/src/main/java/wary/bhaskar/myto_doapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/wary/bhaskar/myto_doapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,11 @@
-package wary.bhaskar.myto_doapp
+package wary.bhaskar.myto_doapp.fragments.update
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import wary.bhaskar.myto_doapp.R
 
 class UpdateFragment : Fragment() {
     override fun onCreateView(
